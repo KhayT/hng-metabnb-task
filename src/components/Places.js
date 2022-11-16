d69bd57efa70ab0a85fac9d67eaedf6f49441c64
KhayT <tkprof4@gmail.com>
2022-11-15 23:34:40 -0800

    fix: made places page responsive

--- src/components/Places.js
+++ src/components/Places.js
@@ -2,10 +2,28 @@ import React from "react";
 import "../styles/Places.scss";
 import Inspiration from "./Inspiration";
 import { inspirations } from "./InspirationData";
+import location from "../images/location.png";
 
 const Places = () => {
   return (
     <>
+      <div className="filter">
+        <ul className="filter-list">
+          <li>Resturant</li>
+          <li>Cottage</li>
+          <li>Castle</li>
+          <li>fantast city</li>
+          <li>beach</li>
+          <li>Carbins</li>
+          <li>off-grid</li>
+          <li>Farm</li>
+        </ul>
+
+        <div className="filter-location">
+          <p>Location</p>
+          <img src={location} alt="" />
+        </div>
+      </div>
       <section className="places">
         {inspirations.map((insp) => {
           return <Inspiration inspiration={insp} key={insp.id} />;
