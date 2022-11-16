f1c6e9aca398093911ac34a23341912aea038424
KhayT <tkprof4@gmail.com>
2022-11-15 17:05:54 -0800

    feature: added connect wallet feature

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,30 +1,53 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+import "../styles/Modal.scss";
 import cross from "../images/x.svg";
 import meta from "../images/meta-mask.svg";
 import wallet from "../images/walletConnect.svg";
 import arrow from "../images/arrow-right.svg";
 
-const Modal = () => {
+const Backdrop = () => {
+  return <div className="backdrop"></div>;
+};
+
+const ModalOverlay = (props) => {
   return (
     <div className="modal">
-      <div>
+      <div className="header">
         <h1>Connect Wallet</h1>
-        <img src={cross} alt="" />
+        <img src={cross} alt="" onClick={props.closeModal} />
       </div>
-      <div>
-        <p>choose your preferred Wallet</p>
-        <div>
-          <img src={meta} alt="" />
+      <div className="options">
+        <p className="title">choose your preferred Wallet:</p>
+        <div className="option">
+          <img src={meta} alt="" className="logo" />
           <p>Metamask</p>
-          <img src={arrow} alt="" />
+          <img src={arrow} alt="" className="arrow" />
         </div>
-        <div>
-          <img src={wallet} alt="" />
+        <div className="option">
+          <img src={wallet} alt="" className="logo" />
           <p>WalletConnect</p>
-          <img src={arrow} alt="" />
+          <img src={arrow} alt="" className="arrow" />
         </div>
       </div>
     </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdrop-root")
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalOverlay closeModal={props.closeModal} />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
-// import classes from "../styles/NavBar.scss";
 import "../styles/NavBar.scss";
+import Modal from "./Modal";
 
 const NavBar = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const connectWallteHandler = () => {
+    setOpenModal(true);
+  };
+
+  const closeModalHandler = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <nav className="nav">
-      <img src={logo} alt="logo" />
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/place-to-stay">Place to stay</Link>
-        </li>
-        <li>
-          <Link to="/">NFTs</Link>
-        </li>
-        <li>
-          <Link to="/">Community</Link>
-        </li>
-      </ul>
-      <button>Connect Wallet</button>
-    </nav>
+    <>
+      <nav className="nav">
+        <img src={logo} alt="logo" />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/place-to-stay">Place to stay</Link>
+          </li>
+          <li>
+            <Link to="/">NFTs</Link>
+          </li>
+          <li>
+            <Link to="/">Community</Link>
+          </li>
+        </ul>
+        <button onClick={connectWallteHandler}>Connect Wallet</button>
+      </nav>
+
+      {openModal ? <Modal closeModal={closeModalHandler} /> : ""}
+    </>
   );
 };
 
